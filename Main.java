2eeafb80f532aac0c85f0a0f478be9e7a5cf4714
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-06 18:52:42 +0530

    Added : First name starts with Cap and has minimum 3 characters

---
+++ Main.java
@@ -0,0 +1,8 @@
+package regex;
+
+public class Main {
+    public static void main(String[] args) {
+        UserRegistration userReg = new UserRegistration();
+        userReg.validFirstName();
+    }
+}
--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,21 @@
 package regex;
 
-public class UserRegistration {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+public void validFirstName(){
+   Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+   Scanner sc = new Scanner(System.in);
+   String firstName = sc.nextLine();
+
+    Matcher matcher = pattern.matcher(firstName);
+    if(matcher.matches()){
+        System.out.println("First name is valid");
+    }else{
+        System.out.println("Not valid");
+    }
+
+    }
 }
