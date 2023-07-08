07f1a5527c1b28ce07981d3a03b90e319f09ab27
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-08 19:53:57 +0530

    Added : Should have at least 1 numeric number in password

---
+++ Main.java
@@ -0,0 +1,12 @@
+package regex;
+
+public class Main {
+    public static void main(String[] args) {
+        UserRegistration userReg = new UserRegistration();
+        /*userReg.validFirstName();
+        userReg.validLastName();
+        userReg.validEmailName();
+        userReg.validPhoneNumber();*/
+        userReg.validPassword();
+    }
+}
